abfff18c5f7415e14b6aa5a3632fcd3e6e1bece0
Nicolas Kruchten <nicolas@datacratic.com>
2015-12-24 19:57:11 +0100

    added watch to gulpfile

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,11 @@
 require('require-dir')('./gulp');
 
 var gulp = require('gulp');	
+
+gulp.task('watch', function() {
+  gulp.watch('./*.coffee', ['makeJs']);
+  gulp.watch('./dist/pivot.css', ['makeCss']);
+});
+
 gulp.task('default', ['makeJs', 'makeCss']);
 
