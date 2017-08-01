8a0fe2c78fbddc0520317cd862ff85daee3618c4
Nicolas Kruchten <nicolas@datacratic.com>
2017-08-01 00:04:56 -0400

    moving *coffee to src

--- gulpFile.js
+++ gulpFile.js
@@ -24,7 +24,7 @@ gulp.task('makeCss', function() {
 
 gulp.task('makeJs', function() {
 
-    gulp.src(['./*.coffee', './locales/*.coffee', './tests/*.coffee'])
+    gulp.src(['./src/*.coffee', './locales/*.coffee', './tests/*.coffee'])
         //compile to js (and create map files)
         .pipe(sourcemaps.init())
         .pipe(coffee()).on('error', gutil.log)
