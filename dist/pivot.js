1abf234f1844094060cda1cbfe5904aeaae81d78
Domeshow <domeshowemmanuel@gmail.com>
2023-05-08 16:24:19 +0200

    [ENH] Drag and drop on mobile without touchpunch

--- dist/pivot.js
+++ dist/pivot.js
@@ -1754,6 +1754,74 @@
     };
 
     /*
+    Support Touch Event
+     */
+    var supportTouch = function () {
+        $.support.touch = true;
+
+        if (!$.support.touch) {
+            return;
+        }
+
+        var proto = $.ui.mouse.prototype,
+            _mouseInit = proto._mouseInit;
+
+        $.extend(proto, {
+            _getElementToBind: function() {
+                var el = this.element;
+                return el;
+            },
+
+            _mouseInit: function() {
+                this._getElementToBind().bind("touchstart." + this.widgetName, $.proxy(this, "_touchStart"));
+                _mouseInit.apply(this, arguments);
+            },
+
+            _touchStart: function(event) {
+                if (event.originalEvent.targetTouches.length != 1) {
+                    return false;
+                }
+
+                if (!this._mouseCapture(event, false)) {
+                    return true;
+                }
+
+                this.element
+                    .bind("touchmove." + this.widgetName, $.proxy(this, "_touchMove"))
+                    .bind("touchend." + this.widgetName, $.proxy(this, "_touchEnd"));
+
+                this._modifyEvent(event);
+
+                $(document).trigger($.Event("mouseup"));
+                this._mouseDown(event);
+
+                return false;
+            },
+
+            _touchMove: function(event) {
+                this._modifyEvent(event);
+                this._mouseMove(event);
+            },
+
+            _touchEnd: function(event) {
+                this.element
+                    .unbind("touchmove." + this.widgetName)
+                    .unbind("touchend." + this.widgetName);
+                this._mouseUp(event);
+            },
+
+            _modifyEvent: function(event) {
+                event.which = 1;
+                var target = event.originalEvent.targetTouches[0];
+                event.pageX = target.clientX;
+                event.pageY = target.clientY;
+            }
+
+        });
+    }
+    supportTouch();
+
+    /*
     Barchart post-processing
      */
     return $.fn.barchart = function(opts) {
